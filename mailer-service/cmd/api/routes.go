4b8c2d1af3733188624ebfa34fa41f7ebb26100c
Stephen Davies <ext-stephen.davies@lianatech.com>
2022-06-29 13:19:09 +0300

    Adding mailer service skekleton

---
+++ mailer-service/cmd/api/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"net/http"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "ContentType", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+	mux.Use(middleware.Heartbeat("/ping"))
+	// mux.Post("/log", app.WriteLog)
+	return mux
+}
