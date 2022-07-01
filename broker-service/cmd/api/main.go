0e635a13a7a4a05eb4736cf16b866c6ac5a94edd
Stephen Davies <ext-stephen.davies@lianatech.com>
2022-07-01 11:52:40 +0300

    adding basic dependencies

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/rabbitmq/amqp091-go"
 	"log"
+	"math"
 	"net/http"
+	"os"
+	"time"
 )
 
 const webPort = "8081"
 
-type Config struct{}
+type Config struct {
+	Rmq *amqp091.Connection
+}
 
 func main() {
-	a := Config{}
+	rc, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rc.Close()
+
+	a := Config{
+		Rmq: rc,
+	}
+
 	log.Printf("Starting Broker Service on port: %s\n", webPort)
 
 	s := &http.Server{
@@ -19,9 +35,40 @@ func main() {
 		Handler: a.routes(),
 	}
 
-	err := s.ListenAndServe()
+	err = s.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
 	}
+
+}
+
+func connect() (*amqp091.Connection, error) {
+	var x int64
+	var bko = 1 * time.Second
+	var con *amqp091.Connection
+
+	for {
+		c, err := amqp091.Dial("amqp://guest:guest@rabbitmq:5672")
+		if err != nil {
+			fmt.Println("RabbitMQ not ready...")
+			x++
+		} else {
+			log.Println("Connected to RabbitMQ")
+			con = c
+			break
+		}
+
+		if x > 5 {
+			fmt.Printf("something went wrong %s\n", err)
+			return nil, err
+		}
+
+		bko = time.Duration(math.Pow(float64(x), 2)) * time.Second
+		log.Println("backing off...")
+		time.Sleep(bko)
+		continue
+	}
+
+	return con, nil
 
 }
